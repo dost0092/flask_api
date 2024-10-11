1e82abdbd4c69273b8e11448bceb2b6d5eb482e4
dost@0092 <waqasdostdost0092@gmail.com>
2024-10-11 03:14:47 -0700

    Update API to include update functionality of pages on 100 on each page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -21,28 +21,32 @@ def connect_pdb():
 
 
 @app.route("/")
-def getdata():
+def get_all_cities():
+   
+    page = request.args.get("page", default=1, type=int) 
+    limit = request.args.get("limit", default=100, type=int)  
+
+    # skipped pages number
+    offset = (page - 1) * limit  
+
     conn = connect_pdb()
     if conn is None:
         return jsonify({"error": "Database connection failed"}), 500
 
     try:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM cities_20241010_muz LIMIT 500;")  # Corrected typo
-        state = cursor.fetchall()
+        cursor.execute("SELECT * FROM cities_20241010_muz LIMIT %s OFFSET %s;", (limit, offset))
+        cities = cursor.fetchall()
         
-        
-
         cursor.close()
         conn.close()
         
-        return jsonify(state)  
+        return jsonify(cities)
     
     except psycopg2.Error as e:
         print(f"Database query error: {e}")
         return jsonify({"error": "Database query failed"}), 500
-
-
+    
 
 
 @app.route("/<int:city_id>", methods=["GET"])
