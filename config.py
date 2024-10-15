7257ac9d3031a40206ee0f7c250b0438bcab0288
dost@0092 <waqasdostdost0092@gmail.com>
2024-10-14 22:24:45 -0700

    config file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    DB_HOST = os.getenv("DB_HOST", "localhost")  # Default to localhost
-    DB_NAME = os.getenv("DB_NAME", "Zones")      # Default database name
-    DB_USER = os.getenv("DB_USER", "postgres")   # Default user
-    DB_PASS = os.getenv("DB_PASS", "dost")       # Default password
+    DB_HOST = os.getenv("DB_HOST", "") 
+    DB_NAME = os.getenv("DB_NAME", "")   
+    DB_USER = os.getenv("DB_USER", "")   
+    DB_PASS = os.getenv("DB_PASS", "")       
